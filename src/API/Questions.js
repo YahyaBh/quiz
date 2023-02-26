f10cebd4ffb1a696992d10946c9781b70583c614
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-02-26 12:47:24 +0100

    Update Questions.js

--- src/API/Questions.js
+++ src/API/Questions.js
@@ -1,7 +1,7 @@
 export const quiz = {
     topic: 'Safer Internet',
     level: 'Medium',
-    totalQuestions: 1,
+    totalQuestions: 20,
     perQuestionScore: 10,
     questions: [
         {
@@ -10,6 +10,123 @@ export const quiz = {
             choices: ['un outil de comminucation', 'le web', 'réseaux mondiaux interconnectés', 'Tout ce qui précède'],
             type: 'MCQs',
             correctAnswer: 'réseaux mondiaux interconnectés',
-        },
+        }, {
+            question:
+                "What is the cybersecurity",
+            choices: ['defending againnst viruses', 'attacking other systems', 'protecting servers,computers... against cyberattacks', 'Rien'],
+            type: 'MCQs',
+            correctAnswer: 'protecting servers,computers... against cyberattacks',
+        }, {
+            question:
+                "When the virus of love released",
+            choices: ['1900', '2001', '2007', '1999'],
+            type: 'MCQs',
+            correctAnswer: '1999',
+        }, {
+            question:
+                "What is the average number of cyberattacks happening per day",
+ 
+
+            choices: ['2220', '2000', '30000', '4223'],
+            type: 'MCQs',
+            correctAnswer: '2220',
+        }, {
+            question:
+                "كيف احمي نفسي في الانترنت ؟",
+            choices: ['استخدام متصفح قوي', 'استخدام شبكات اتصال عامة', 'تحميل برامج من مصادر مشبوهة', 'عمل تحذيثات للجهاز'],
+            type: 'MCQs',
+            correctAnswer: 'عمل تحذيثات للجهاز',
+        }, {
+            question:
+                "What is a virus ?",
+            choices: ['prorgrammed application', 'type of malicious code or program', 'decoded passwords', 'application of malware'],
+
+            type: 'MCQs',
+            correctAnswer: 'type of malicious code or program',
+        }, {
+            question:
+                "Qu'est-ce qu'un firewall ?",
+            choices: ['matériel et logiciel qui sépare un réseau local', 'logiciel qui sépare un réseau local', 'matériel qui sépare un réseau local', 'Un application de securité'],
+            type: 'MCQs',
+            correctAnswer: 'matériel et logiciel qui sépare un réseau local',
+        }, {
+            question:
+                "Comment puis-je connaître un site Web frauduleux ??",
+            choices: ["en utilisant l'url du site", "à l'aide d'une application de sécurité", 'Assurez-vous que le domaine correspond au certificat SSL', 'Tout ce qui précède'],
+            type: 'MCQs',
+            correctAnswer: 'Tout ce qui précède',
+        }, {
+            question:
+                "HTTP signifie",
+            choices: ['hyper text transfer protocol', 'hypertet transmission protocol', 'high transfer transport protocol', 'hyper transfer text protocol'],
+            type: 'MCQs',
+            correctAnswer: 'hyper text transfer protocol',
+        }, {
+            question:
+                "We access the World Wide Web using",
+            choices: ['Browsers', 'Instant messaging applications', 'High bandwidth', 'Search engine'],
+            type: 'MCQs',
+            correctAnswer: 'Browsers',
+        }, {
+            question:
+                "_____جهازان كمبيوتر أو أكثر متصلان ببعضهما البعض يتشاركان المعلومات من ",
+            choices: ['شبكة', 'جهاز التوجيه', 'الخادم', 'نفق'],
+            type: 'MCQs',
+            correctAnswer: 'شبكة',
+        }, {
+            question:
+                "What is the full form of WWW?",
+            choices: ['World Wide Web', 'World With Web', 'Work Wide Web', 'World Wide Wet'],
+            type: 'MCQs',
+            correctAnswer: 'World Wide Web',
+        }, {
+            question:
+                "The design of the network is called the network:",
+            choices: ['architecture', 'server', 'transmission', 'type'],
+            type: 'MCQs',
+            correctAnswer: 'architecture',
+        }, {
+            question:
+                "Programs such as Mozila Firefox that serve as navigable windows onto the Web are called ___",
+            choices: ['Hypertext', 'Networks', 'Internet', 'Web browsers'],
+            type: 'MCQs',
+            correctAnswer: 'Web browsers',
+        }, {
+            question:
+                "Le serveur sur Internet est également connu sous le nom de :",
+            choices: ['Répétitrice', 'hôtesse', 'passerelle', 'Porte ET'],
+            type: 'MCQs',
+            correctAnswer: 'hôtesse',
+        }, {
+            question:
+                "Le site Web est une collection de:",
+            choices: ['HTML documents', 'Fichiers graphiques', 'Fichiers audio et vidéo', 'Tout ce qui précède'],
+            type: 'MCQs',
+            correctAnswer: 'Tout ce qui précède',
+        }, {
+            question:
+                "Which of the following is not a network ?",
+            choices: ['Local Area Network', 'Wide Area Network', 'Optical Fibre', 'All are networks'],
+            type: 'MCQs',
+            correctAnswer: 'Optical Fibre',
+        }, {
+            question:
+                "Sending email contain a suspicious application is_____",
+            choices: ['Phishing', 'Scamming', 'CyberCrime', 'All of the above'],
+            type: 'MCQs',
+            correctAnswer: 'All of the above',
+        }, {
+            question:
+                "DSL est un exemple de connexion a(n)",
+            choices: ['network', 'wireless', 'slow', 'broadband'],
+            type: 'MCQs',
+            correctAnswer: 'broadband',
+        }, {
+            question:
+                "Qu'est-ce que le domaine de premier niveau de code de pays Internet (ccTLD) pour les îles de Tuvalu ?",
+            choices: ['.ta', '.tu', '.tl', '.tv'],
+            type: 'MCQs',
+            correctAnswer: '.tv',
+        }
     ],
 }
